c980d1473114e85cc0c6bdcf7b018c5799fc5b3e
benstephens56 <49702729+benstephens56@users.noreply.github.com>
2026-03-05 11:54:25 -0500

    Add files via upload

---
+++ encore/encore/src/core/hle/service/ssl/ssl_c.h
@@ -0,0 +1,43 @@
+// Copyright 2014 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <random>
+#include <boost/serialization/binary_object.hpp>
+#include "core/hle/service/service.h"
+
+namespace Core {
+class System;
+}
+
+namespace Service::SSL {
+
+class SSL_C final : public ServiceFramework<SSL_C> {
+public:
+    SSL_C();
+
+private:
+    void Initialize(Kernel::HLERequestContext& ctx);
+    void GenerateRandomData(Kernel::HLERequestContext& ctx);
+
+    std::minstd_rand deterministic_random_gen{};
+
+    template <class Archive>
+    void serialize(Archive& ar, const unsigned int) {
+        ar& boost::serialization::base_object<Kernel::SessionRequestHandler>(*this);
+        ar& boost::serialization::make_binary_object(&deterministic_random_gen,
+                                                     sizeof(deterministic_random_gen));
+    }
+
+    friend class boost::serialization::access;
+};
+
+void InstallInterfaces(Core::System& system);
+
+void GenerateRandomData(std::vector<u8>& out);
+
+} // namespace Service::SSL
+
+BOOST_CLASS_EXPORT_KEY(Service::SSL::SSL_C)
